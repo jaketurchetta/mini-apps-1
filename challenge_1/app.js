202fee58c524e8468e41f8adf0137547ee5286e9
Jake Turchetta <14jturchetta@gmail.com>
2020-04-20 20:48:51 -0400

    started advanced content

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -11,6 +11,18 @@ var oScore = 0;
 let charX = false;
 let gameOver = false;
 
+// Enter player names
+const enterName = () => {
+  let nameX = prompt('Please enter name for player 1 (X\'s)');
+  playerX = nameX;
+  document.getElementById("playerX").innerText = 'X - ' + playerX + ':';
+  let nameO = prompt('Please enter name for player 2 (O\'s)');
+  playerO = nameO;
+  document.getElementById("playerO").innerText = 'O - ' + playerO + ':';
+}
+
+enterName();
+
 // Flip between X's and O's
 const chooseChar = () => {
   if (!charX) {
@@ -46,78 +58,78 @@ const searchForWin = () => {
   if ((board[0] + board[1] + board[2] === 'XXX') || (board[0] + board[1] + board[2] === 'OOO')) {
     endGame();
     if (board[0] = 'X') {
-      alert(playerOne + ' is the winner!');
-      xScore++;
+      document.getElementById("xScore").innerText = Number(document.getElementById("xScore").innerText.innerText)++;
+      alert(playerX + ' is the winner!');
     } else {
-      alert(playerTwo + ' is the winner!');
-      oScore++;
+      oScore.innerText = Number(oScore.innerText)++;
+      alert(playerO + ' is the winner!');
     }
   } else if ((board[3] + board[4] + board[5] === 'XXX') || (board[3] + board[4] + board[5] === 'OOO')) {
     endGame();
     if (board[0] = 'X') {
-      alert(playerOne + ' is the winner!');
-      xScore++;
+      document.getElementById("xScore").innerText.innerText = Number(document.getElementById("xScore").innerText.innerText)++;
+      alert(playerX + ' is the winner!');
     } else {
-      alert(playerTwo + ' is the winner!');
-      oScore++;
+      oScore.innerText = Number(oScore.innerText)++;
+      alert(playerO + ' is the winner!');
     }
   } else if ((board[6] + board[7] + board[8] === 'XXX') || (board[6] + board[7] + board[8] === 'OOO')) {
     endGame();
     if (board[0] = 'X') {
-      alert(playerOne + ' is the winner!');
-      xScore++;
+      document.getElementById("xScore").innerText.innerText = Number(document.getElementById("xScore").innerText.innerText)++;
+      alert(playerX + ' is the winner!');
     } else {
-      alert(playerTwo + ' is the winner!');
-      oScore++;
+      oScore.innerText = Number(oScore.innerText)++;
+      alert(playerO + ' is the winner!');
     }
   }
   // Vertcal wins
   else if ((board[0] + board[3] + board[6] === 'XXX') || (board[0] + board[3] + board[6] === 'OOO')) {
     endGame();
     if (board[0] = 'X') {
-      alert(playerOne + ' is the winner!');
-      xScore++;
+      document.getElementById("xScore").innerText.innerText = Number(document.getElementById("xScore").innerText.innerText)++;
+      alert(playerX + ' is the winner!');
     } else {
-      alert(playerTwo + ' is the winner!');
-      oScore++;
+      oScore.innerText = Number(oScore.innerText)++;
+      alert(playerO + ' is the winner!');
     }
   } else if ((board[1] + board[4] + board[7] === 'XXX') || (board[1] + board[4] + board[7] === 'OOO')) {
     endGame();
     if (board[1] = 'X') {
-      alert(playerOne + ' is the winner!');
-      xScore++;
+      document.getElementById("xScore").innerText.innerText = Number(document.getElementById("xScore").innerText.innerText)++;
+      alert(playerX + ' is the winner!');
     } else {
-      alert(playerTwo + ' is the winner!');
-      oScore++;
+      oScore.innerText = Number(oScore.innerText)++;
+      alert(playerO + ' is the winner!');
     }
   } else if ((board[2] + board[5] + board[8] === 'XXX') || (board[2] + board[5] + board[8] === 'OOO')) {
     endGame();
     if (board[2] = 'X') {
-      alert(playerOne + ' is the winner!');
-      xScore++;
+      document.getElementById("xScore").innerText.innerText = Number(document.getElementById("xScore").innerText.innerText)++;
+      alert(playerX + ' is the winner!');
     } else {
-      alert(playerTwo + ' is the winner!');
-      oScore++;
+      oScore.innerText = Number(oScore.innerText)++;
+      alert(playerO + ' is the winner!');
     }
   }
   // Diagonal wins
   if ((board[0] + board[4] + board[8] === 'XXX') || (board[0] + board[4] + board[8] === 'OOO')) {
     endGame();
     if (board[0] = 'X') {
-      alert(playerOne + ' is the winner!');
-      xScore++;
+      document.getElementById("xScore").innerText.innerText = Number(document.getElementById("xScore").innerText.innerText)++;
+      alert(playerX + ' is the winner!');
     } else {
-      alert(playerTwo + ' is the winner!');
-      oScore++;
+      oScore.innerText = Number(oScore.innerText)++;
+      alert(playerO + ' is the winner!');
     }
   } else if ((board[2] + board[4] + board[6] === 'XXX') || (board[2] + board[4] + board[6] === 'OOO')) {
     endGame();
     if (board[2] = 'X') {
-      alert(playerOne + ' is the winner!');
-      xScore++;
+      document.getElementById("xScore").innerText.innerText = Number(document.getElementById("xScore").innerText .innerText)++;
+      alert(playerX + ' is the winner!');
     } else {
-      alert(playerTwo + ' is the winner!');
-      oScore++;
+      oScore.innerText = Number(oScore.innerText)++;
+      alert(playerO + ' is the winner!');
     }
   }
 }
